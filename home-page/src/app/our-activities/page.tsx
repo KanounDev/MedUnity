6453ee334a65a9db9265f06475f3261efdd3234d
Peeeekaa123 <khaledkanoun10@gmail.com>
2025-11-02 10:35:18 +0100

    7th commit

---
+++ home-page/src/app/our-activities/page.tsx
@@ -0,0 +1,92 @@
+
+export default function Activities() {
+  return (
+    <main className="activities-page">
+      <section className="hero-banner">
+        <h1>OUR ACTIVITIES</h1>
+      </section>
+
+      <section className="content">
+        <h2>UN INSTITUT DE PATHOLOGIE À VOTRE SERVICE …</h2>
+        <div className="divider"></div>
+
+        <div className="activity-section">
+          <div className="map">
+             <img src="/im1.png" alt="Carte Paris-Lille" />
+          </div>
+          <div className="text">
+            <h3>… Entre Paris et Lille</h3>
+            <p>
+              L’Institut de Pathologie des Hauts-de-France prend en charge des actes d’histologie et de cytologie et dispose d’une technique innovante de biologie moléculaire pour le génotypage des virus HPV dans le cadre du dépistage du cancer du col de l’utérus.
+            </p>
+            <p>
+              L’Institut de Pathologie a une activité étendue à toute la région des Hauts-de-France ainsi qu’à Paris, la région parisienne, la Normandie, le Cotentin et la Bretagne, au sein d’établissements privés et hospitaliers et auprès de nombreux praticiens libéraux.
+            </p>
+          </div>
+        </div>
+        <div className="activity-section4">
+          <div className="map4">
+            <img src="/im4.png" alt="Carte Paris-Lille" />
+          </div>
+
+          <div className="text4">
+            <h3>IMMUNOHISTOCHIMIE … Anticorps recherche antigène</h3>
+            <p>
+              L’immunohistochimie consiste à évaluer la réactivité des cellules à des anticorps dans le but de mieux caractériser les tumeurs pour mettre en place le traitement le plus adapté.</p>
+            <p>
+              Nous disposons d’un très large panel d’anticorps que nous évaluons régulièrement en fonction des données scientifiques publiées pour nous permettre une caractérisation très précise des tumeurs et des traitements auxquels elles seront sensibles.</p>
+          </div>
+        </div>
+        <div className="activity-section2">
+          <div className="map2">
+            <img src="/im2.png" alt="Carte Paris-Lille" />
+          </div>
+
+          <div className="text2">
+            <h3>… Pour permettre des résultats rapides et contrôlés</h3>
+            <p>
+             Les résultats des examens sont le plus souvent disponibles 24h à 48h après leur réception. Un exemplaire est transmis au médecin qui a réalisé le prélèvement et le plus souvent au médecin traitant. Le patient peut également recevoir un exemplaire.
+             Les pathologistes de l’Institut de Pathologie des Hauts-de-France participent aux réunions de concertations pluri-disciplinaires (RCP) au sein des établissements de soin où sont pris en charge les patients.</p>
+            <p>
+              En cas de difficulté diagnostique, d’étroites relations avec des pathologistes experts français permettent de bénéficier de leur expérience.
+              Dans le cadre de la médecine personnalisée, l’Institut de Pathologie des Hauts-de-France collabore avec le service de biologie moléculaire des Centres Hospitaliers Universitaires d’Amiens et de Lille pour la réalisation des tests de sensibilité thérapeutique en oncologie et les recherches en génétique.</p>
+          </div>
+        </div>
+        <div className="activity-section3">
+          <div className="map3">
+            <img src="/im3.png" alt="Carte Paris-Lille" />
+          </div>
+
+          <div className="text3">
+            <h3>HISTOLOGIE … Voir en grand l’infiniment petit</h3>
+            <p>
+            L’histologie est l’analyse des tissus, successivement à l’œil nu puis au microscope. Elle est pratiquée à partir de :
+              <ul>
+                <li>Biopsies (sein, prostate, estomac, côlon, peau, …)</li>
+                <li>Petites exérèses (polypes, lésions cutanées, …)</li>
+                <li>Pièces opératoires correspondant à l’exérèse partielle ou complète d’un ou plusieurs organes (sein, vessie, prostate, côlon, …)</li>
+              </ul>
+              Seul l’examen histologique par un médecin pathologiste permet d’affirmer un diagnostic de cancer.
+              L’équipe médicale est organisée en pôles de compétence avec des pathologistes spécialisés dans certains domaines de la spécialité pour permettre une prise en charge optimale de l’ensemble des prélèvements qui nous sont confiés.
+              </p>
+          </div>
+        </div>
+        <div className="activity-section5">
+          <div className="map5">
+            <img src="/im5.png" alt="Carte Paris-Lille" />
+          </div>
+
+          <div className="text5">
+            <h3>BIOLOGIE MOLECULAIRE … Ou la Médecine Personnalisée</h3>
+            <p>
+             La biologie moléculaire consiste à caractériser la molécule responsable de l’état pathologique.
+                La biologie moléculaire est réalisée à partir des prélèvements confiés aux pathologistes. Biologie moléculaire et Anatomie-Pathologique sont ainsi étroitement liées.
+                
+              Nous disposons d’un très large panel d’anticorps que nous évaluons régulièrement en fonction des données scientifiques publiées pour nous permettre une caractérisation très précise des tumeurs et des traitements auxquels elles seront sensibles.</p>
+            <p>L’ensemble de ces recherches est actuellement pratiqué au sein de plateformes de génétique moléculaire mises en place par l’Institut National du Cancer (INCa). A l’Institut de Pathologie des Hauts-de-France, nous collaborons avec les plateformes régionales (Amiens et Lille) pour réduire les délais de prise en charge thérapeutique des patients.</p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
