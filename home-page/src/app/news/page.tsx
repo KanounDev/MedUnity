dc3e65c86e2a8fe99e50bc0eca2c4ea02d789174
Morsi0 <morsi.feki.officiel@gmail.com>
2025-11-12 14:21:03 +0100

    home page + news

---
+++ home-page/src/app/news/page.tsx
@@ -0,0 +1,9 @@
+import NewsSection from "@/components/NewsSection";
+
+export default function Page() {
+  return (
+    <main>
+      <NewsSection />
+    </main>
+  );
+}
--- home-page/src/app/page.tsx
+++ home-page/src/app/page.tsx
@@ -1,18 +1,14 @@
 import Link from 'next/link';
 import { HeartIcon } from '@heroicons/react/24/solid';
+import HomePage from "@/components/HomePage";
 import styles from './page.module.css';
 
-export default function Home() {
+export default function Page() {
   return (
     <div className={styles.hero}>
-      <HeartIcon className={styles.icon} />
-      <h1 className={styles.title}>MedUnity</h1>
-      <p className={styles.subtitle}>Votre laboratoire de pathologie de confiance</p>
-      <div className={styles.actions}>
-        <Link href="/our-activities" className={styles.btn}>Nos Activités</Link>
-        <Link href="/team" className={styles.btn}>Notre Équipe</Link>
-        <Link href="/contact" className={styles.btnOutline}>Nous Contacter</Link>
-      </div>
+      <main>
+        <HomePage />
+      </main>
     </div>
   );
 }
