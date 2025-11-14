f0fbccab017f7082d0e7199ef0fe572372a03ee2
Mohamed KANOUN <113695061+KanounDev@users.noreply.github.com>
2025-11-14 21:50:13 +0100

    Delete espase-dod directory

--- espase-dod/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Footer.module.css";
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      © 2025 MedUnity — All rights reserved.
-    </footer>
-  );
-}
